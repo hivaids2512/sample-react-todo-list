eb928168613abc3f33ee9b8f2351ac5645e3475d
Quy Vu <tranquy2512@gmail.com>
2017-07-14 00:44:29 +0700

    added Todo item component

---
+++ src/components/Todo/TodoList/TodoItem/TodoItemView.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class TodoItemView extends Component {
+
+    constructor(){       
+    }
+
+    render() {
+        return (
+            <a href="#" onClick={() => this.props.switchTodo(this.props.todo)} className="list-group-item"> {this.props.order} . {this.props.todo.name} </a>
+        );
+    }
+
+}
--- src/components/Todo/TodoList/TodoListView.jsx
+++ src/components/Todo/TodoList/TodoListView.jsx
@@ -1,38 +1,39 @@
-import React, { Component } from 'react';
-import Emitter from 'helpers/emitter';
-import AddTodoContainer from '../AddTodo/AddTodoContainer';
+import React, { Component } from "react";
+import Emitter from "helpers/emitter";
+import AddTodoContainer from "../AddTodo/AddTodoContainer";
+import TodoItem from "./TodoItem/TodoItemView";
 
 export default class TodoListPresentation extends Component {
+  constructor() {
+    super();
+  }
 
-    constructor() {
-        super();
-    }
+  switchTodo(todo) {
+    Emitter.emit("todoChanged", todo);
+  }
 
-    switchTodo(todo) {
-        Emitter.emit('todoChanged', todo);
-    }
-
-    render() {
-        let context = this;
-        return (
-            <div className="col-md-3">
-                <div className="row">
-                    <div className="col-md-6">
-                        <p className="lead">Your Todos </p>
-                    </div>
-                    <div className="col-md-6">
-                        <span className="pull-right"> <AddTodoContainer /></span>
-                    </div>
-                </div>
-                <div className="list-group">
-                    {
-                        this.props.todoList.map(function (todo, i) {
-                            return <a href="#" key={i} onClick={() => context.switchTodo( todo )} className="list-group-item"> {i + 1}. {todo.name} </a>;
-                        })
-                    }
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    let context = this;
+    return (
+      <div className="col-md-3">
+        <div className="row">
+          <div className="col-md-6">
+            <p className="lead">Your Todos </p>
+          </div>
+          <div className="col-md-6">
+            <span className="pull-right"> <AddTodoContainer /></span>
+          </div>
+        </div>
+        <div className="list-group">
+          {this.props.todoList.map(function(todo, i) {
+            <TodoItemView
+              todo={todo}
+              order={i}
+              onSwitchTodo={context.switchTodo}
+            />;
+          })}
+        </div>
+      </div>
+    );
+  }
 }
